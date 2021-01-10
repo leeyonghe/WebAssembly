ca48ae65fd1651cdeee8ec75816820f155149af8
lee <lyhdra99@svcorps.dev>
2021-01-10 18:48:32 +0900

    init

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
+const path = require("path");
 const port = 8000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const ejs = require('ejs');
 
 app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  ejs.renderFile(path.resolve(__dirname,'./public/views/main.html'), null, null, (err, str) => !err && res.send(str))
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
