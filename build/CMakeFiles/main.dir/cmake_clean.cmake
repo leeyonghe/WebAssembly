c16085e2c33ae3f7c8c18f9e06d103ab9f01d518
lee <lyhdra99@svcorps.dev>
2021-01-10 18:20:20 +0900

    init

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../public/main.html"
+  "../public/main.pdb"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
